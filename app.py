c54411b2c0ce71c9f9b8a85f6a398d8eb0e382db
Lohan Victor <lohanvictor@proton.me>
2026-08-17 23:36:10 -0300

    CRUD completo

--- app.py
+++ app.py
@@ -7,32 +7,68 @@ st.title("esse é um titulo")
 st.header("esse é um cabeçalho")
 st.subheader("esse é um cabeçalho menor")
 
-with st.form("nome_do_formulario"):
+#Cadastrando a nota dos alunos
+with st.form("cadastrar_aluno"):
     nome = st.text_input("Nome")
     idade = st.number_input("Idade", value = 50)
-    nota = st.number_input("Nota", value= 0.0, step= 0.5, min_value= 0.0, max_value= 10.0)
+    nota = st.number_input("Nota", value = 0.0, step= 0.5, min_value= 0.0, max_value= 10.0)
 
-
-    #cargo = st.text_input("Cargo")
-    #dt_nasc = st.date_input("Campo data", value="today")
 
     btn_form = st.form_submit_button("Enviar")
 
+#Inserindo dados no banco de dados
 if btn_form:
-    conn = db.conectar() #conecto
-    cursor = conn.cursor() #crio o cursor
+    db.criar_aluno(nome, idade, nota)
 
-    cursor.execute("INSERT INTO alunos(nome, idade, nota) VALUES (?, ?, ?)", (nome, idade, nota))
-
-    conn.commit() #commito o que eu fiz
-    conn.close() #fecho a conexão
-    
-
-
-#if btn_form:
+    #if btn_form:
 #    st.write("Seus dados foram enviados com sucesso.")
 #    st.write(f"Nome: {nome}")
 #    st.write(f"Idade: {idade}")
 #    st.write(f"Nota: {nota}")
 #    st.write(f"Cargo: {cargo}")
 #    st.write(f"Data de nasicmento: {dt_nasc}")
+    
+#Procurando alunos
+with st.form("buscar_aluno"):
+    nome = st.text_input("Nome do aluno")
+    btn_buscar = st.form_submit_button("Buscar")
+
+if btn_buscar:
+
+    alunos = db.buscar_aluno(nome)
+
+    if alunos:
+        st.dataframe(
+            alunos,
+            column_config={ #Puramente visual. É o nome que fica em cada campo da tabela
+                1: "ID",
+                2: "Nome",
+                3: "Idade",
+                4: "Nota"
+            },
+            hide_index=True
+        )
+
+    else:
+        st.write("Nenhum aluno encontrado.")
+
+#Editando alunos
+with st.form("editar_aluno"):
+    id = st.text_input("ID do Aluno a ser alterado")
+    nome = st.text_input("Novo NOME")
+    idade = st.number_input("Nova IDADE", value = 50)
+    nota = st.number_input("Nova NOTA", value = 0.0, step= 0.5, min_value= 0.0, max_value= 10.0)
+
+    btn_form = st.form_submit_button("Editar")
+
+    if btn_form:
+        db.editar_aluno(id, nome, idade, nota)
+
+#Deletando alunos
+with st.form("deletar_aluno"):
+    id = st.text_input("ID do Aluno a ser deletado")
+
+    btn_form = st.form_submit_button("Deletar")
+
+    if btn_form:
+        db.deletar_aluno(id)
--- database.py
+++ database.py
@@ -4,7 +4,9 @@ def conectar():
     conn = sqlite3.connect("escola.db")
     return conn 
 
-def criar_tabela():
+
+#Fazendo as funções do CRUD
+def criar_tabela(): 
 
     conn = conectar()
     cursor = conn.cursor()
@@ -19,3 +21,54 @@ def criar_tabela():
 
     conn.commit() 
     conn.close() 
+
+#CREATE: Criando a tabela alunos
+def criar_aluno(nome, idade, nota):
+    conn = conectar() #conecto
+    cursor = conn.cursor() #crio o cursor
+
+    cursor.execute("INSERT INTO alunos(nome, idade, nota) VALUES (?, ?, ?)", (nome, idade, nota))
+
+    conn.commit() #commito o que eu fiz
+    conn.close() #fecho a conexão
+
+
+#READ: Procurando e exibindo alunos no banco de dados
+def buscar_aluno(nome):
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM alunos WHERE nome = ?",
+        (nome,)
+    )
+
+    alunos = cursor.fetchall()
+
+    conn.close()
+    return alunos
+
+#UPDATE: Alterando informações dos alunos
+def editar_aluno(id, nome, idade, nota):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE alunos
+        SET nome = ?, idade = ?, nota = ?
+        WHERE ID = ?
+    """, (nome, idade, nota, id))
+
+    conn.commit()
+    conn.close()
+
+#DELETE: SUMINDO com os alunos chatos pra cacete
+def deletar_aluno(id):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM alunos WHERE ID = ?", (id,))
+
+    conn.commit()
+    conn.close()
